97aa30d799ccab1c4b8965f4ba35790f71e9a4aa
todor19836 <todorvalkov0@gmail.com>
2026-01-12 16:39:41 +0200

    change name

---
+++ Zadacha8/Zadacha8.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Task8
+{
+    static void Main()
+    {
+        int[] count = new int[6];
+        Random rnd = new Random();
+
+        for (int i = 0; i < 100000; i++)
+            count[rnd.Next(6)]++;
+
+        for (int i = 0; i < 6; i++)
+            Console.WriteLine($"{i + 1}: {(double)count[i] / 100000}");
+    }
+}
